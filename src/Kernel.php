523f99b73682ac5d80d6ef2f7f10f6a8d68b15c0
Thomas <thomas@sctr.net>
2025-06-08 14:59:18 -0500

    fixed style

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Distribution and reproduction are prohibited.
+ *
+ * @package   thebreathingnook
+ * @copyright Deluxetom 2025
+ * @license   No License (Proprietary)
+ */
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
