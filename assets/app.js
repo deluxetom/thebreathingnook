ab0833f51415c8f13aa8f628e2c963ecbe63eef7
Thomas <thomas@sctr.net>
2025-06-14 10:20:12 -0500

    more color tests

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").classList.add("bg-dark-500", "backdrop-blur-md");
+        document.getElementById("header").classList.add("bg-body-800/90", "backdrop-blur-md");
     } else {
-        document.getElementById("header").classList.remove("bg-dark-500", "backdrop-blur-md");
+        document.getElementById("header").classList.remove("bg-body-800/90", "backdrop-blur-md");
     }
 }
 
