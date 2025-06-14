abfb92080902c9c6a4370e60ccf45f9c51447815
Thomas <thomas@sctr.net>
2025-06-13 23:14:02 -0500

    more green changes

--- assets/app.js
+++ assets/app.js
@@ -9,8 +9,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").classList.add("bg-dark-500", "backdrop-blur-md", "shadow-lg");
+        document.getElementById("header").classList.add("bg-dark-500", "backdrop-blur-md");
     } else {
-        document.getElementById("header").classList.remove("bg-dark-500", "backdrop-blur-md", "shadow-lg");
+        document.getElementById("header").classList.remove("bg-dark-500", "backdrop-blur-md");
     }
 }
+
+ //when document loads, check the current scroll position and set the header class accordingly
+document.addEventListener("DOMContentLoaded", function() {
+    scrollFunction();
+});
