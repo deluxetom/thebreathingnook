0a7b746b9155c7863e5aab50d8319a91d772fb68
Thomas <thomas@sctr.net>
2025-06-13 22:57:12 -0500

    blue

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,13 @@ import 'flowbite';
 //import 'flowbite-datepicker';
 
 AOS.init();
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").classList.add("bg-dark-500", "backdrop-blur-md", "shadow-lg");
+    } else {
+        document.getElementById("header").classList.remove("bg-dark-500", "backdrop-blur-md", "shadow-lg");
+    }
+}
