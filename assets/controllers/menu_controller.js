174acfe711d34ac0371b7e2f4bc6c884260e57dd
Thomas <thomas@sctr.net>
2025-06-13 23:44:42 -0500

    fixed mobile menu

---
+++ assets/controllers/menu_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from '@hotwired/stimulus';
+import {useClickOutside, useTransition} from 'stimulus-use';
+
+export default class extends Controller {
+    static targets = ['hamburger', 'menu'];
+
+    connect() {
+        useClickOutside(this);
+        useTransition(this, {
+            element: this.menuTarget,
+            enterActive: 'transition ease-out duration-300',
+            enterFrom: 'transform -translate-x-1/2',
+            enterTo: 'transform translate-x-0',
+            leaveActive: 'transition ease-out duration-300',
+            leaveFrom: 'transform translate-x-0',
+            leaveTo: 'transform -translate-x-full',
+        });
+    }
+
+    close() {
+        this.leave();
+        document.body.style.overflowY = 'visible';
+    }
+
+    open() {
+        this.enter();
+        document.body.style.overflowY = 'hidden';
+    }
+
+    toggle() {
+        this.toggleTransition();
+    }
+
+    clickOutside() {
+        this.close();
+    }
+}
